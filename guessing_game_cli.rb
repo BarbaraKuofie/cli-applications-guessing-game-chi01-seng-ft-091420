f63d7e7fa4490c02c282e735dd304d273a4567ce
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 14:00:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ user_input = gets.chomp
  ## binding.pry 
 if user_input == "exit"
  puts "Goodbye!"
-elsif user_input == random 
+elsif user_input == random.to_s
   puts "You guessed the correct number!"
  else 
   puts "Sorry! The computer guessed #{random}."
