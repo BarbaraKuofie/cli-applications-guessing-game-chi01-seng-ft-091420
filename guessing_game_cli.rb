eb55dfd1c1c2e4d90fee4b7967779e9778a55c39
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-09-14 13:55:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
 random = rand(1..6).to_s
 puts "Guess a number between 1 and 6"
 user_input = gets.chomp
- binding.pry 
+ ## binding.pry 
 if user_input == "exit"
  return  "Goodbye!"
 elsif user_input == random 
